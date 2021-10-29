f9961dbb86daa154142660aac6703cabc3c0f28e
Sidi Mohammed Moustafa Bounoua <bounoua.smm@gmail.com>
2021-10-29 21:43:59 +0000

    -added vendor from production

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInitf86cfc4e9158a8cf4cd1d417725376d7
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitf86cfc4e9158a8cf4cd1d417725376d7', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInitf86cfc4e9158a8cf4cd1d417725376d7', 'loadClassLoader'));
